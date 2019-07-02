3bfdc357d6276124ee5f75dff6724d585dfab8fd
AlbertNthiwa <albertnthiwa@gmail.com>
2019-07-02 17:22:36 +0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
+
+const routes = [
+  {path:'', component: NavigationComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
